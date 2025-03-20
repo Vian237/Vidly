995ebf022347752a661a7da8c16c1b17144a75c7
Vianney Ngaleu <vianneyngaleu@gmail.com>
2025-03-20 16:30:17 +0100

    Action Result

--- Vidly/Controllers/MoviesController.cs
+++ Vidly/Controllers/MoviesController.cs
@@ -13,6 +13,9 @@ namespace Vidly.Controllers
                 Name = "Shrek!"
             };
             return View(movie);
+            //return Content("Hello Vianney");
+            //return new EmptyResult();
+            //return RedirectToAction("Index", "Home", new {page = 1, sortBy = "name"});
         }
     }
 }
