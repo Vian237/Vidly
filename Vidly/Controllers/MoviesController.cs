8debd2d85137e43f303149c2c4774341f2e22d00
Vianney Ngaleu <vianneyngaleu@gmail.com>
2025-03-20 16:46:22 +0100

    ActionsParameter

--- Vidly/Controllers/MoviesController.cs
+++ Vidly/Controllers/MoviesController.cs
@@ -17,5 +17,22 @@ namespace Vidly.Controllers
             //return new EmptyResult();
             //return RedirectToAction("Index", "Home", new {page = 1, sortBy = "name"});
         }
+
+        public IActionResult Edit(int id)
+        {
+            return Content("id=" + id);
+        }
+
+        //movies
+        public IActionResult Index(int? pageIndex, string sortBy)
+        {
+            if (!pageIndex.HasValue)
+                pageIndex = 1;
+
+            if (string.IsNullOrWhiteSpace(sortBy))
+                sortBy = "Name";
+
+            return Content($"pageIndex={pageIndex} & sortBy={sortBy}");
+        }
     }
 }
