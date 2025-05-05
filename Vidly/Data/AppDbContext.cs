e39459f60dec0d43c4d4fdfcfa18115f44bc085a
Vianney Ngaleu <vianneyngaleu@gmail.com>
2025-05-05 19:30:59 +0200

    Enable Migration

---
+++ Vidly/Data/AppDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Vidly.Models;
+
+namespace Vidly.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public DbSet<Customer> Customers { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            var cn = @"Server=.\SQLEXPRESS; Database=CapBioRent; Trusted_Connection=True;TrustServerCertificate=True;";
+            optionsBuilder.UseSqlServer(cn);
+
+            base.OnConfiguring(optionsBuilder);
+        }
+    }
+
+}
