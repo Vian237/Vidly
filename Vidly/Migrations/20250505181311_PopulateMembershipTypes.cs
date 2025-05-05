9988944410529302779b6673471e60dec6c8b76e
Vianney Ngaleu <vianneyngaleu@gmail.com>
2025-05-05 20:22:25 +0200

    Add Data to MembershipType table

---
+++ Vidly/Migrations/20250505181311_PopulateMembershipTypes.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Vidly.Migrations
+{
+    /// <inheritdoc />
+    public partial class PopulateMembershipTypes : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            //Inserer les valeurs dans le tableau de cette facon pour s'assuer que selon les versions du code on aura la mm valeur pour ce tableau
+            migrationBuilder.Sql("INSERT INTO MembershipType (Id, SignUpFee, DurationInMonths, DiscountRate) VALUES (1,0,0,0)");
+            migrationBuilder.Sql("INSERT INTO MembershipType (Id, SignUpFee, DurationInMonths, DiscountRate) VALUES (2,30,1,10)");
+            migrationBuilder.Sql("INSERT INTO MembershipType (Id, SignUpFee, DurationInMonths, DiscountRate) VALUES (3,90,3,15)");
+            migrationBuilder.Sql("INSERT INTO MembershipType (Id, SignUpFee, DurationInMonths, DiscountRate) VALUES (4,300,12,20)");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
