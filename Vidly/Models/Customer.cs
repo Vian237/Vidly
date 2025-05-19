39a7c8be7eedaa4c584c5eb0fa8c44b2ca60200b
Vianney Ngaleu <vianneyngaleu@gmail.com>
2025-05-19 23:32:44 +0200

    Update label Name of customer property

--- Vidly/Models/Customer.cs
+++ Vidly/Models/Customer.cs
@@ -1,9 +1,12 @@
-﻿namespace Vidly.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Vidly.Models
 {
     public class Customer
     {
         public int Id { get; set; }
         public string Name { get; set; }
+        [Display(Name = "Date of Birth")]
         public DateTime? Birthdate { get; set; }
         public bool IsSubscribedToNewsletter { get; set; }
 
