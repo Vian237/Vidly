786621b6472f165bbc491b409a24059804b9397e
Vianney Ngaleu <vianneyngaleu@gmail.com>
2025-06-02 14:18:51 +0200

    Building an API for Customer

---
+++ Vidly/Controllers/Api/CustomersController.cs
@@ -0,0 +1,91 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Vidly.Data;
+using Vidly.Models;
+
+namespace Vidly.Controllers.Api
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CustomersController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+
+        public CustomersController()
+        {
+            _context = new AppDbContext();
+        }
+
+        //Get: api/customers
+        public IEnumerable<Customer> GetCustomers()
+        {
+            return _context.Customers.Include(c => c.MembershipType).ToList();
+        }
+
+        //Get: api/customers/5
+        [HttpGet("{id}")]
+        public ActionResult<Customer> GetCustomer(int id)
+        {
+            var customer = _context.Customers.Include(c => c.MembershipType).SingleOrDefault(c => c.Id == id);
+            if (customer == null)
+                return NotFound();
+
+            return customer;
+        }
+
+        //Post: api/customers
+        [HttpPost]
+        public ActionResult<Customer> CreateCustomer(Customer customer)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            _context.Customers.Add(customer);
+            _context.SaveChanges();
+
+            // Return the created customer with a 201 Created response
+            //return CreatedAtAction(nameof(GetCustomer), new { id = customer.Id }, customer);
+            return customer;
+        }
+
+        //Put: api/customers/5
+        [HttpPut("{id}")]
+        public ActionResult UpdateCustomer(int id, Customer customer)
+        {
+            if (id != customer.Id)
+                return BadRequest();
+
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            var customerInDb = _context.Customers.SingleOrDefault(c => c.Id == id);
+
+            if (customerInDb == null)
+                return NotFound();
+
+            customerInDb.Name = customer.Name;
+            customerInDb.Birthdate = customer.Birthdate;
+            customerInDb.IsSubscribedToNewsletter = customer.IsSubscribedToNewsletter;
+            customerInDb.MembershipTypeId = customer.MembershipTypeId;
+
+            _context.SaveChanges();
+            return NoContent();
+        }
+
+        //Delete: api/customers/5
+        [HttpDelete("{id}")]
+        public ActionResult DeleteCustomer(int id)
+        {
+            var customerInDb = _context.Customers.SingleOrDefault(c => c.Id == id);
+            if (customerInDb == null)
+                return NotFound();
+
+            _context.Customers.Remove(customerInDb);
+            _context.SaveChanges();
+
+            // Return a 204 No Content response to indicate successful deletion
+            return NoContent();
+        }
+    }
+}
